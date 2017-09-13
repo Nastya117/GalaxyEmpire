c222f8cea07ab97b6f29d9d4fabfe671d0e3e293
Anastasya Sidorova <nastya@MacBook-Pro-Anastasya.local>
2017-09-13 17:10:28 +0300

    Хуй

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
 extern crate jpeg_decoder as jpeg;
+extern crate rgb;
 
+
+use rgb::*;
 use std::fs::File;
 use std::io::BufReader;
 
@@ -13,29 +16,41 @@ fn main()
     let mut decoder = jpeg::Decoder::new(BufReader::new(file));
     let pixels = decoder.decode().expect("failed to decode image");
     let metadata = decoder.info().unwrap();
+    let pixels = pixels.as_slice().as_rgb();
 
     let hi = metadata.height as usize;
     let wi = metadata.width as usize;
 
     let mut mat1 = Vec::new();
-    let mut mat = Vec::new();
+    let mut mat2 = Vec::new();
+    let mut mat3 = Vec::new();
+
+    let mut matr = Vec::new();
+    let mut matg = Vec::new();
+    let mut matb = Vec::new();
     
     for i in 0..hi
     {
         for j in 0..wi
         {
-            mat1.push(pixels[i * wi + j]);
+            mat1.push(pixels[i * wi + j].r);
+            mat2.push(pixels[i * wi + j].g);
+            mat3.push(pixels[i * wi + j].b);
         }
-        mat.push(mat1.clone());
+        matr.push(mat1.clone());
         mat1.clear();
+        matg.push(mat2.clone());
+        mat2.clear();
+        matb.push(mat3.clone());
+        mat3.clear();
     }
 
 
-    for i in 0..hi
+     for i in 0..hi
     {
         for j in 0..wi
         {
-            print!("{}", mat[i][j]);
+            print!("{} ", matb[i][j]);
         }
         println!("");
     }
